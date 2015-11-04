a0e00714d34bdf4c9f2eeaad1ca34d612870bc70
Daniel Llinas <danllinas@gmail.com>
2015-11-03 19:23:28 -0500

    Messed with the JS.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,20 +1,19 @@
 var ready;
 ready = function() {
   var navbar = $('nav');
-  var origOffsetY = $('.header-image').outerHeight(true);
-  console.log(origOffsetY);
+  var origOffsetY = 683//$('.header-image').outerHeight(true);
+
 
   function scroll() {
-      if ($(window).scrollTop() >= origOffsetY) {
-          $('nav').addClass('navbar-fixed-top');
-          $('.content').addClass('menu-padding');
-      } else {
-          $('nav').removeClass('navbar-fixed-top');
-          $('.content').removeClass('menu-padding');
-      }
-
-
+    if ($(window).scrollTop() >= origOffsetY) {
+      $('nav').addClass('navbar-fixed-top');
+      // $('.content').addClass('menu-padding');
+    } else {
+      $('nav').removeClass('navbar-fixed-top');
+      // $('.content').removeClass('menu-padding');
+    }
   }
+
   document.onscroll = scroll;
 };
 
