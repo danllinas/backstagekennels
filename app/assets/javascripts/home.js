3e783d9cf069035da21a9122450b287279ecf1ee
Daniel Llinas <danllinas@gmail.com>
2015-11-10 19:51:15 -0500

    Updated landing page.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,21 +1,21 @@
-var ready;
-ready = function() {
-  var navbar = $('nav');
-  var origOffsetY = 683//$('.header-image').outerHeight(true);
-
-
-  function scroll() {
-    if ($(window).scrollTop() >= origOffsetY) {
-      $('nav').addClass('navbar-fixed-top');
-      // $('.content').addClass('menu-padding');
-    } else {
-      $('nav').removeClass('navbar-fixed-top');
-      // $('.content').removeClass('menu-padding');
-    }
-  }
-
-  document.onscroll = scroll;
-};
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
+// var ready;
+// ready = function() {
+//   var navbar = $('nav');
+//   var origOffsetY = 1019//$('header').height();
+//   //console.log(origOffsetY);
+//
+//   function scroll() {
+//     if ($(window).scrollTop() >= origOffsetY) {
+//       $('nav').addClass('navbar-fixed-top');
+//       $('.content').addClass('menu-padding');
+//     } else {
+//       $('nav').removeClass('navbar-fixed-top');
+//       // $('.content').removeClass('menu-padding');
+//     }
+//   }
+//
+//   document.onscroll = scroll;
+// };
+//
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
