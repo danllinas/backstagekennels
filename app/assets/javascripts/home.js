c6a6859738260ea05c618e2a62eac9e97c2a70a7
Daniel Llinas <danllinas@gmail.com>
2015-11-30 20:08:49 -0500

    Updated JavaScript for smooth scroll and fix.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,21 +1,18 @@
-// var ready;
-// ready = function() {
-//   var navbar = $('nav');
-//   var origOffsetY = 1019//$('header').height();
-//   //console.log(origOffsetY);
-//
-//   function scroll() {
-//     if ($(window).scrollTop() >= origOffsetY) {
-//       $('nav').addClass('navbar-fixed-top');
-//       $('.content').addClass('menu-padding');
-//     } else {
-//       $('nav').removeClass('navbar-fixed-top');
-//       // $('.content').removeClass('menu-padding');
-//     }
-//   }
-//
-//   document.onscroll = scroll;
-// };
-//
-// $(document).ready(ready);
-// $(document).on('page:load', ready);
+var ready;
+ready = function() {
+
+};
+
+scroll = function (){
+  var height = $("header").height();
+
+  if ($(window).scrollTop() >= height){
+    $("nav").addClass("navbar-fixed-top");
+  } else {
+    $("nav").removeClass("navbar-fixed-top");
+  }
+};
+
+$(document).scroll(scroll);
+$(document).ready(ready);
+$(document).on('page:load', ready);
