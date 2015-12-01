2a5ab397f2e0d1100a38bad81ea13e4cccbaaf5a
Daniel Llinas <danllinas@gmail.com>
2015-12-01 08:31:08 -0500

    Updated the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :posts
+  get "litters" => 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
