05c76af9be941fbeae5113df0e9c285d4814965d
Daniel Llinas <danllinas@gmail.com>
2015-12-07 09:47:51 -0500

    Finished uploader for admin and conditional links for editing and deleting posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -10,13 +10,31 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to "litters"
+    redirect_to "puppies"
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    flash[:success] = "Post successfully updated."
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:image, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :posts
-  get "litters" => 'posts#index'
+  get "puppies" => 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
