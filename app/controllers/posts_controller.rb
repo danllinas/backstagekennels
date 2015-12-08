9875d59226f02cf1185b9c8e95bbc55614adb1b2
Daniel Llinas <danllinas@gmail.com>
2015-12-08 11:18:19 -0500

    Putting the finishing touches. Added devise authentication for client login. Fixed some route naming.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: :new
   def index
     @posts = Post.all
   end
