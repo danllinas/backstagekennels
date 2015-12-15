fde0025fa4e348d3f0ac9c2893230f22b0e9ae7c
Daniel Llinas <danllinas@gmail.com>
2015-12-14 20:27:07 -0500

    Removed scrolling navbar for better mobile presentation.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,18 +1,18 @@
-var ready;
-ready = function() {
-
-};
-
-scroll = function (){
-  var height = $("header").height();
-
-  if ($(window).scrollTop() >= height){
-    $("nav").addClass("navbar-fixed-top");
-  } else {
-    $("nav").removeClass("navbar-fixed-top");
-  }
-};
-
-$(document).scroll(scroll);
-$(document).ready(ready);
-$(document).on('page:load', ready);
+// var ready;
+// ready = function() {
+//
+// };
+//
+// scroll = function (){
+//   var height = $("header").height();
+//
+//   if ($(window).scrollTop() >= height){
+//     $("nav").addClass("navbar-fixed-top");
+//   } else {
+//     $("nav").removeClass("navbar-fixed-top");
+//   }
+// };
+//
+// $(document).scroll(scroll);
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
